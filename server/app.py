30e12f9e0fd9e176f423e7779cf1076c3b868b2b
mzb527 <mzb527@gmail.com>
2025-05-31 04:39:04 -0400

    Uploading Lab

--- server/app.py
+++ server/app.py
@@ -1,20 +1,72 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
-
 from config import app, db, api
 from models import User, UserSchema
 
-class ClearSession(Resource):
+user_schema = UserSchema()
 
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+
+        if not username or not password:
+            return {"error": "Username and password required"}, 400
+
+        if User.query.filter_by(username=username).first():
+            return {"error": "Username already taken"}, 409
+
+        user = User(username=username)
+        user.password_hash = password  # Hashing handled in setter method
+
+        db.session.add(user)
+        db.session.commit()
+
+        session["user_id"] = user.id  # Log user in
+        return user_schema.dump(user), 201
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not user.authenticate(password):
+            return {"error": "Invalid credentials"}, 401
+
+        session["user_id"] = user.id
+        return user_schema.dump(user), 200
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get("user_id")
+
+        if not user_id:
+            return {}, 204
+
+        user = User.query.get(user_id)
+        return user_schema.dump(user), 200
+
+class Logout(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session["user_id"] = None
         return {}, 204
 
+class ClearSession(Resource):
+    def delete(self):
+        session["page_views"] = None
+        session["user_id"] = None
+        return {}, 204
+
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,28 +1,27 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from marshmallow import Schema, fields
-
 from config import db, bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    _password_hash = db.Column(db.String)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
 
-    # Build method to protect password_hash property
+    # Protect password_hash property
     @hybrid_property
     def password_hash(self):
-        pass
+        raise AttributeError("Password hashes cannot be accessed directly.")
 
-    # Build method to set password hash property using bcrypt.generate_password_hash()
+    # Set password hash property using bcrypt
     @password_hash.setter
     def password_hash(self, password):
-        pass
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    # Build authenticate method that uses bcrypt.check_password_hash()
+    # Authenticate method to verify password
     def authenticate(self, password):
-        pass
+        return bcrypt.check_password_hash(self._password_hash, password)
 
     def __repr__(self):
         return f'User {self.username}, ID: {self.id}'
